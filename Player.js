2eaad51aa4b3bb39a688f5dc4dc8f87c579ce20a
andrithstef <andrithstef@gmail.com>
2021-11-02 12:47:14 +0000

    meira sprite dót komið

--- Player.js
+++ Player.js
@@ -12,8 +12,10 @@ Player.prototype.GO_LEFT = 'A'.charCodeAt(0);
 Player.prototype.GO_RIGHT = 'D'.charCodeAt(0); 
 Player.prototype.GO_UP = 'W'.charCodeAt(0); 
 Player.prototype.GO_DOWN = 'S'.charCodeAt(0); 
+Player.prototype.AIM_UP = 16; //SHIFT
+Player.prototype.AIM_DOWN = 17; //CTRL
 Player.prototype.JUMP = " ".charCodeAt(0);
-Player.prototype.SHOOT = 13;
+Player.prototype.SHOOT = 13; //ENTER
 
 Player.prototype.gravity = 0.6;
 Player.prototype.accel = 1.5;
@@ -37,10 +39,15 @@ Player.prototype.Ydirection = 1;
 
 Player.prototype.shape = "Rect";
 
-Player.prototype.stance = "Standing";
+Player.prototype.stance = 1;
 Player.prototype.animationFrame = 0;
 Player.prototype.framenr = 0;
-Player.prototype.framestoAnimationFrame = 6;
+Player.prototype.framestoAnimationFrame = 5;
+
+Player.prototype.bulletX = this.cx;
+Player.prototype.bulletY = this.cy;
+Player.prototype.bulletVelX = 1;
+Player.prototype.bulletVelY = 0;
 
 Player.prototype.update = function(du){
     spatialManager.unregister(this);
@@ -99,19 +106,7 @@ Player.prototype.update = function(du){
     var nextX = this.cx + this.velX * du;
 
 
-    if (this.velX < 0.01 && this.velX > -0.01){
-        this.velX = 0;
-    }
-
-    if (this.velX === 0){
-        this.stance = 1;
-    }
-    else if (this.velX > 0){
-        this.stance = 2;
-    }
-    else if (this.velX < 0){
-        this.stance = 3;
-    }
+    this.getStance();
 
     //Check collisions with floor
     var dir = Math.sign(this.velY);
@@ -165,34 +160,90 @@ Player.prototype.hitsMap = function(x,y){
 }
 
 Player.prototype.shoot = function(){
-    if ((g_keys[this.GO_UP] && g_keys[this.GO_DOWN]) 
-    || (!g_keys[this.GO_UP] && !g_keys[this.GO_DOWN] )){
+    if ((g_keys[this.AIM_UP] && g_keys[this.AIM_DOWN]) 
+    || (!g_keys[this.AIM_UP] && !g_keys[this.AIM_DOWN] )){
         this.Ydirection = 0;
     }
-    else if (g_keys[this.GO_UP]){
+    else if (g_keys[this.AIM_UP]){
         this.Ydirection = -1;
     }
-    else if (g_keys[this.GO_DOWN]){
+    else if (g_keys[this.AIM_DOWN]){
         this.Ydirection = 1;
     }
     this.Ydirection = Math.sign(this.Ydirection);
-    entityManager.addBullet(this.cx, this.cy, this.Xdirection, this.Ydirection);
+    entityManager.addBullet(this.bulletX, this.bulletY, this.Xdirection, this.Ydirection);
+}
+
+Player.prototype.getStance = function(){
+    if (this.velX < 0.01 && this.velX > 0){
+        //Standing still, looking right
+        if (g_keys[this.AIM_UP]){
+            this.stance = 4;
+        }
+        else if (g_keys[this.AIM_DOWN]){
+            this.stance = 5;
+        }
+        else{
+            this.stance = 1;
+        }
+    }
+
+    else if (this.velX < 0 && this.velX > -0.01){
+        //Standing still, looking left
+        if (g_keys[this.AIM_UP]){
+            this.stance = 6;
+        }
+        else if (g_keys[this.AIM_DOWN]){
+            this.stance = 7;
+        }
+        else{
+            this.stance = 0;
+        }
+    }
+    else if (this.velX > 0){
+        this.stance = 2;
+    }
+    else if (this.velX < 0){
+        this.stance = 3;
+    }
 }
 
 Player.prototype.getSprite = function(){
     if (this.stance === 1){
-        this.halfHeight = 50;
-        this.halfWidth = 23;
+        //Looking right
+        this.halfHeight = 42;
+        this.halfWidth = 24;
+
+        this.bulletX  = this.cx + 10;
+        this.bulletY = this.cy - 9;
         return{
-            x : 732,
-            y : 193,
-            w : 23,
-            h : 50
+            x : 243,
+            y : 18,
+            w : 24,
+            h : 42
+        } 
+    }
+    else if (this.stance === 0){
+        //looking left
+        this.halfHeight = 42;
+        this.halfWidth = 25;
+
+        this.bulletX  = this.cx - 10;
+        this.bulletY = this.cy - 9;
+        return{
+            x : 242,
+            y : 77,
+            w : 25,
+            h : 42
         }
     }
     else if (this.stance === 2){
+        //running right
         this.halfHeight = 41;
-        this.halfWidth = 17;
+        this.halfWidth = 34;
+
+        this.bulletX  = this.cx + 10;
+        this.bulletY = this.cy - 9;
         return{
             x : this.dists[0][this.animationFrame],
             y : 267,
@@ -201,12 +252,72 @@ Player.prototype.getSprite = function(){
         } 
     }
     else if (this.stance === 3){
+        //running left
         this.halfHeight = 41;
-        this.halfWidth = 17;
+        this.halfWidth = 34;
+
+        this.bulletX  = this.cx - 10;
+        this.bulletY = this.cy - 9;
         return{
             x : this.dists[1][this.animationFrame],
             y : 325,
             w : this.widths[1][this.animationFrame],
+            h : 41
+        } 
+    }
+    else if (this.stance === 4){
+        //Aiming right, up
+        this.halfHeight = 47;
+        this.halfWidth = 31;
+
+        this.bulletX  = this.cx - 8;
+        this.bulletY = this.cy - 7;
+        return{
+            x : 110,
+            y : 13,
+            w : 31,
+            h : 47
+        } 
+    }
+    else if (this.stance === 5){
+        //Aiming right, down
+        this.halfHeight = 41;
+        this.halfWidth = 30;
+
+        this.bulletX  = this.cx + 18;
+        this.bulletY = this.cy - 6;
+        return{
+            x : 504,
+            y : 19,
+            w : 30,
+            h : 41
+        } 
+    }
+    else if (this.stance === 6){
+        //Aiming left, up
+        this.halfHeight = 48;
+        this.halfWidth = 31;
+
+        this.bulletX  = this.cx - 23;
+        this.bulletY = this.cy - 40;
+        return{
+            x : 102,
+            y : 71,
+            w : 31,
+            h : 48
+        } 
+    }
+    else if (this.stance === 7){
+        //Aiming left, down
+        this.halfHeight = 41;
+        this.halfWidth = 30;
+
+        this.bulletX  = this.cx - 22;
+        this.bulletY = this.cy;
+        return{
+            x : 504,
+            y : 78,
+            w : 30,
             h : 41
         } 
     }
